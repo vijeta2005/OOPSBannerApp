90c93d4ff27dc97924c5866252d88726a3d607d7
vijeta2005 <vs2991@srmist.edu.in>
2026-03-02 08:33:17 +0530

    [Vijeta] Added java file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,24 @@
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        String[] lines = new String[9];
+        System.out.println();
+        lines[0] = String.join("   ","   *****   ","   *****   ","*******  ","    *****   ");
+        lines[1] = String.join("   "," **     ** "," **     ** ","**     **","  **     **  ");
+        lines[2] = String.join("   ","**       **","**       **","**      **","**         ");
+        lines[3] = String.join("   ","**       **","**       **","**     ** ","  **        ");
+        lines[4] = String.join("   ","**       **","**       **","*******   ","    ***    ");
+        lines[5] = String.join("   ","**       **","**       **","**        ","        **  ");
+        lines[6] = String.join("   ","**       **","**       **","**        ","         ** ");
+        lines[7] = String.join("   "," **     ** "," **     ** ","**        "," **     **  ");
+        lines[8] = String.join("   ","   *****   ","   *****   ","**        ","   *****   ");
+        System.out.println();
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+
+        System.out.println();
+    }
+}
